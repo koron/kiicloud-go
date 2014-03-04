2e89dce5309e5473d01ffa402f1964b7ea43c6aa
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-03-04 23:58:35 +0900

    implmengt UserClient constructor

--- kiicloud/user.go
+++ kiicloud/user.go
@@ -1,11 +1,56 @@
 package kiicloud
 
+import (
+	"fmt"
+	"net/http"
+)
+
 type UserClient struct {
 	// All inherit from Client.
 	Client
 }
 
-func NewUserClient(entryPoint, appId, appKey, userName, password string) (*UserClient, error) {
-	// TODO: implement me.
-	return nil, nil
+// NewUserClient create a new user client.
+func NewUserClient(entryPoint, appId, appKey, loginName, password string) (*UserClient, error) {
+	c := &UserClient{Client {entryPoint, appId, appKey, "", false}}
+
+	var err error
+	c.Authorization, err = c.authorize(loginName, password)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+// authorize assure user login.
+func (c *UserClient) authorize(loginName, password string) (string, error) {
+	// Create a request object.
+	reqobj := newMap(nil)
+	reqobj["username"] = loginName
+	reqobj["password"] = password
+
+	authorization := ""
+	err := c.Send("/oauth2/token", "POST", reqobj,
+		"application/vnd.kii.OauthTokenRequest+json",
+		func(resp *http.Response) error {
+			switch resp.StatusCode {
+			case 200:
+				var respobj OauthTokenResponse
+				err := parseJson(resp.Body, &respobj)
+				if err != nil {
+					return err
+				}
+				authorization = fmt.Sprintf("%s %s", respobj.TokenType,
+					respobj.AccessToken)
+				return nil
+			default:
+				return ToError(resp)
+			}
+		})
+	if err != nil {
+		return "", err
+	}
+
+	return authorization, nil
 }
